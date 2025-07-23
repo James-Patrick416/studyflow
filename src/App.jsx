12f3f4fc82046bccb83cc1e192127f93cf4fb738
ann ngatia <annngatia94@gmail.com>
2025-07-23 10:24:27 +0300

    Resolve merge conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import { Routes, Route } from 'react-router-dom';
 import './styles/App.css';
 import NavBar from './components/NavBar';
-import Home from './pages/Home.jsx';
-import Pomodoro from './pages/Pomodoro.jsx';
-import Custom from './pages/Custom.jsx';
-import DeepWork from './pages/DeepWork.jsx';
-import Stats from './pages/Stats.jsx';
+import Home from './pages/Home';
+import Pomodoro from './pages/Pomodoro';
+import Custom from './pages/Custom';
+import DeepWork from './pages/DeepWork';
+import Stats from './pages/Stats';
 
 export default function App() {
   return (
-      <div className="app-container">
-        <NavBar />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/pomodoro" element={<Pomodoro />} />
-            <Route path="/custom" element={<Custom />} />
-            <Route path="/deepwork" element={<DeepWork />} />
-            <Route path="/stats" element={<Stats />} />
-            <Route path="*" element={<h2>404 - Page Not Found</h2>} />
-          </Routes>
-        </main>
-      </div> 
+    <div className="app-container">
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pomodoro" element={<Pomodoro />} />
+          <Route path="/custom" element={<Custom />} />
+          <Route path="/deepwork" element={<DeepWork />} />
+          <Route path="/stats" element={<Stats />} />
+          <Route path="*" element={<h2>404 - Page Not Found</h2>} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
